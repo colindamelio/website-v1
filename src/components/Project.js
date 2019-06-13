3b200825d8e981f835ee4e32e847628be256e45f
colindamelio <colin.damelio@gmail.com>
2019-06-12 20:36:42 -0400

    for the future

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import Image from './Image';
-import { fadeIn } from '../lib/animations';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Image from "./Image";
+import { fadeIn } from "../lib/animations";
 
 const Container = styled.ul`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  flex-wrap: wrap;
-  margin: 40px auto 0;
-  padding: 0 20px;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	flex-wrap: wrap;
+	margin: 40px auto 0;
+	padding: 0 20px;
 `;
 
 const Item = styled.li`
-  position: relative;
-  max-width: 400px;
-  margin-bottom: 25px;
-  box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
-  animation: ${fadeIn} 5s ease;
+	position: relative;
+	max-width: 400px;
+	margin-bottom: 25px;
+	box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
+	animation: ${fadeIn} 5s ease;
 `;
 
 const Overlay = styled.a`
-  display: ${props => (props.loaded ? 'flex' : 'none')}
+  display: ${props => (props.loaded ? "flex" : "none")}
   flex-direction: column;
   position: absolute;
   right: 0;
@@ -33,47 +33,52 @@ const Overlay = styled.a`
 `;
 
 const Tag = styled.h3`
-  font-size: 13px;
-  margin-top: 0;
+	font-size: 13px;
+	margin-top: 0;
 `;
 
 const Title = styled.h2``;
 const Description = styled.p``;
 
 class Project extends Component {
-  state = {
-    imageLoaded: false,
-  };
+	state = {
+		imageLoaded: false,
+	};
 
-  renderImage = () => {
-    this.setState({
-      imageLoaded: true,
-    });
-  };
+	renderImage = () => {
+		this.setState({
+			imageLoaded: true,
+		});
+	};
 
-  render() {
-    const { items } = this.props;
-    const { imageLoaded } = this.state;
-    return (
-      <Container>
-        {items.map((item, key) => (
-          <Item key={key}>
-            <Image src={item.src} alt={item.alt} onLoad={this.renderImage} />
+	render() {
+		const { items } = this.props;
+		const { imageLoaded } = this.state;
+		return (
+			<Container>
+				{items.map((item, key) => (
+					<Item key={key}>
+						<Image
+							src={item.src}
+							alt={item.alt}
+							onLoad={this.renderImage}
+							loading="lazy"
+						/>
 
-            <Overlay href={item.href} target="_blank" loaded={imageLoaded}>
-              <Title>{item.title}</Title>
-              {item.company && <Tag>{`Built at ${item.company}`}</Tag>}
-              <Description>{item.desc}</Description>
-            </Overlay>
-          </Item>
-        ))}
-      </Container>
-    );
-  }
+						<Overlay href={item.href} target="_blank" loaded={imageLoaded}>
+							<Title>{item.title}</Title>
+							{item.company && <Tag>{`Built at ${item.company}`}</Tag>}
+							<Description>{item.desc}</Description>
+						</Overlay>
+					</Item>
+				))}
+			</Container>
+		);
+	}
 }
 
 Project.propTypes = {
-  items: PropTypes.array,
+	items: PropTypes.array,
 };
 
 export default Project;
--- src/data/index.js
+++ src/data/index.js
@@ -1,76 +1,75 @@
-import saraswati from '../assets/saraswati.jpg';
-import acura from '../assets/acura_homepage.png';
-import acuraBap from '../assets/acura_bap.png';
-import hondaBap from '../assets/honda_bap.png';
-import dealerLocator from '../assets/dealer_locator.png';
+import saraswati from "../assets/saraswati.jpg";
+import acura from "../assets/acura_homepage.png";
+import acuraBap from "../assets/acura_bap.png";
+import hondaBap from "../assets/honda_bap.png";
+import dealerLocator from "../assets/dealer_locator.png";
 
 export const menuItems = [
-  {
-    isProjects: true,
-    title: 'Projects',
-    href: '/',
-    label: 'link to projects page',
-  },
-  {
-    title: 'GitHub',
-    href: 'https://www.github.com/colindamelio',
-    label: 'link to github profile',
-  },
-  {
-    title: 'LinkedIn',
-    href: 'https://www.linkedin.com/in/colindamelio/',
-    label: 'link to linkedin profile',
-  },
-  {
-    title: 'Twitter',
-    href: 'https://twitter.com/clndml',
-    label: 'link to twitter account',
-  },
+	{
+		isProjects: true,
+		title: "Projects",
+		href: "/",
+		label: "Internal link to projects page.",
+	},
+	{
+		title: "GitHub",
+		href: "https://www.github.com/colindamelio",
+		label: "External link to github profile.",
+	},
+	{
+		title: "LinkedIn",
+		href: "https://www.linkedin.com/in/colindamelio/",
+		label: "External link to linkedin profile.",
+	},
+	{
+		title: "Twitter",
+		href: "https://twitter.com/clndml",
+		label: "External link to twitter account.",
+	},
 ];
 
 export const projectItems = [
-  {
-    title: 'Saraswati Bali Retreats',
-    href: 'http://saraswatibali.com/#/',
-    src: saraswati,
-    alt: 'Screen capture of the Saraswati Bali Retreats homepage',
-    desc:
-      'Built with React/React-Router, styled-components.',
-  },
-  {
-    title: 'Acura Homepage',
-    href: 'https://www.acura.ca',
-    src: acura,
-    alt: 'Screen capture of the Acura Canada homepage',
-    company: 'Climax Media',
-    desc:
-      'A responsive digital transformation and redesign of the premium Acura Canada brand. Built with React/Redux, PostCSS, and AODA compliant.',
-  },
-  {
-    title: 'Honda Build & Price',
-    href: 'https://www.honda.ca/buildyourhonda',
-    src: hondaBap,
-    alt: 'Screen capture of the Honda Build and Price app',
-    company: 'Climax Media',
-    desc:
-      'React/Redux application for building & pricing Honda Canada and Acura Canada vehicles. Tooling included React-Router, styled-components, storybook, and a11y. Themed across two product lines.',
-  },
-  {
-    title: 'Acura Build & Price',
-    href: 'https://www.acura.ca/buildyouracura',
-    src: acuraBap,
-    alt: 'Screen capture of the Acura Build and Price app',
-    company: 'Climax Media',
-    desc:
-      'React/Redux application for building & pricing Honda Canada and Acura Canada vehicles. Tooling included React-Router, styled-components, storybook, and a11y. Themed across two product lines.',
-  },
-  {
-    title: 'Honda Dealer Locator',
-    href: 'https://www.honda.ca/dealerlocator',
-    src: dealerLocator,
-    alt: 'Screen capture of the Honda Canada dealer locator application',
-    company: 'Climax Media',
-    desc:
-      'React/Redux application that integrates geo-location and Google Maps Platform to search for Honda/Acura dealerships.',
-  },
+	{
+		title: "Saraswati Bali Retreats",
+		href: "http://saraswatibali.com/#/",
+		src: saraswati,
+		alt: "Screen capture of the Saraswati Bali Retreats homepage",
+		desc: "Built with React/React-Router, styled-components.",
+	},
+	{
+		title: "Acura Homepage",
+		href: "https://www.acura.ca",
+		src: acura,
+		alt: "Screen capture of the Acura Canada homepage",
+		company: "Climax Media",
+		desc:
+			"A responsive digital transformation and redesign of the premium Acura Canada brand. Built with React/Redux, PostCSS, and AODA compliant.",
+	},
+	{
+		title: "Honda Build & Price",
+		href: "https://www.honda.ca/buildyourhonda",
+		src: hondaBap,
+		alt: "Screen capture of the Honda Build and Price app",
+		company: "Climax Media",
+		desc:
+			"React/Redux application for building & pricing Honda Canada and Acura Canada vehicles. Tooling included React-Router, styled-components, storybook, and a11y. Themed across two product lines.",
+	},
+	{
+		title: "Acura Build & Price",
+		href: "https://www.acura.ca/buildyouracura",
+		src: acuraBap,
+		alt: "Screen capture of the Acura Build and Price app",
+		company: "Climax Media",
+		desc:
+			"React/Redux application for building & pricing Honda Canada and Acura Canada vehicles. Tooling included React-Router, styled-components, storybook, and a11y. Themed across two product lines.",
+	},
+	{
+		title: "Honda Dealer Locator",
+		href: "https://www.honda.ca/dealerlocator",
+		src: dealerLocator,
+		alt: "Screen capture of the Honda Canada dealer locator application",
+		company: "Climax Media",
+		desc:
+			"React/Redux application that integrates geo-location and Google Maps Platform to search for Honda/Acura dealerships.",
+	},
 ];
